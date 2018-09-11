e2361ae70ddeda1b0eeddd5b2204a1af7984af47
Rommel Rico <me@rommelrico.com>
2018-09-11 08:21:55 -0700

    Setting the Title of the AddToDoActivity

--- app/src/main/java/com/rommelrico/todolistkotlin/AddToDoActivity.kt
+++ app/src/main/java/com/rommelrico/todolistkotlin/AddToDoActivity.kt
@@ -8,5 +8,8 @@ class AddToDoActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_add_to_do)
+
+        // Setting the title
+        title = "New ToDo"
     }
 }
