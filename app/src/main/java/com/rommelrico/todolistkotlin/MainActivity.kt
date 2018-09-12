78a6607d4bced9d31f2d2000741c5d3be1437aa6
Rommel Rico <me@rommelrico.com>
2018-09-12 08:50:13 -0700

    Removing unused imports in MainActivity

--- app/src/main/java/com/rommelrico/todolistkotlin/MainActivity.kt
+++ app/src/main/java/com/rommelrico/todolistkotlin/MainActivity.kt
@@ -2,11 +2,9 @@ package com.rommelrico.todolistkotlin
 
 import android.content.Intent
 import android.os.Bundle
-import android.support.design.widget.Snackbar
 import android.support.v7.app.AppCompatActivity
 import android.view.Menu
 import android.view.MenuItem
-import io.realm.Realm
 
 import kotlinx.android.synthetic.main.activity_main.*
 
