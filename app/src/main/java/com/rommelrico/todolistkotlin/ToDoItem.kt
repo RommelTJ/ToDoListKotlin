a9f62f8c14a3adf4c82285cb3fd1acff6e3b12e8
Rommel Rico <me@rommelrico.com>
2018-09-12 09:17:22 -0700

    Removing redundant public modifiers

--- app/src/main/java/com/rommelrico/todolistkotlin/ToDoItem.kt
+++ app/src/main/java/com/rommelrico/todolistkotlin/ToDoItem.kt
@@ -3,7 +3,6 @@ package com.rommelrico.todolistkotlin
 import io.realm.RealmObject
 
 open class Dog: RealmObject() {
-    public var name = ""
-    public var age = 0
+    var name = ""
+    var age = 0
 }
-
