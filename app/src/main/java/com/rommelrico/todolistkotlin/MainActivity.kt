f3c890141bf8f301c138a6b972cb32339bfdf0f6
Rommel Rico <me@rommelrico.com>
2018-09-12 08:45:54 -0700

    Initializing Realm

--- app/src/main/java/com/rommelrico/todolistkotlin/MainActivity.kt
+++ app/src/main/java/com/rommelrico/todolistkotlin/MainActivity.kt
@@ -6,6 +6,7 @@ import android.support.design.widget.Snackbar
 import android.support.v7.app.AppCompatActivity
 import android.view.Menu
 import android.view.MenuItem
+import io.realm.Realm
 
 import kotlinx.android.synthetic.main.activity_main.*
 
@@ -20,6 +21,9 @@ class MainActivity : AppCompatActivity() {
             var addIntent = Intent(this, AddToDoActivity::class.java)
             startActivity(addIntent)
         }
+
+        // Realm Testing.
+        Realm.init(this)
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
