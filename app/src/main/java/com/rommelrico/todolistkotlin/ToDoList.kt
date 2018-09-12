c391b65b243f8255fdce78d21497da43b26a7fd7
Rommel Rico <me@rommelrico.com>
2018-09-12 08:50:51 -0700

    Adding parenthesis to Application extension

--- app/src/main/java/com/rommelrico/todolistkotlin/ToDoList.kt
+++ app/src/main/java/com/rommelrico/todolistkotlin/ToDoList.kt
@@ -3,7 +3,7 @@ package com.rommelrico.todolistkotlin
 import android.app.Application
 import io.realm.Realm
 
-public class ToDoList: Application {
+public class ToDoList: Application() {
 
     public override fun onCreate() {
         super.onCreate()
