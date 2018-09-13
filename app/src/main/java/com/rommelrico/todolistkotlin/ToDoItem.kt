d237ada23145bab2f958498e9f02724730b16d2d
Rommel Rico <me@rommelrico.com>
2018-09-13 08:38:59 -0700

    ToDoItem model

--- app/src/main/java/com/rommelrico/todolistkotlin/ToDoItem.kt
+++ app/src/main/java/com/rommelrico/todolistkotlin/ToDoItem.kt
@@ -2,6 +2,11 @@ package com.rommelrico.todolistkotlin
 
 import io.realm.RealmObject
 
+open class ToDoItem: RealmObject() {
+    var name = ""
+    var important = false
+}
+
 open class Dog: RealmObject() {
     var name = ""
     var age = 0
