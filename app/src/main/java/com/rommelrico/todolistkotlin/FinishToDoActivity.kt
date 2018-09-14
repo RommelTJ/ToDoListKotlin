c71124436a160bc2eede605eab9020439d61d0ea
Rommel Rico <me@rommelrico.com>
2018-09-14 08:34:02 -0700

    Adding FinishToDoActivity

---
+++ app/src/main/java/com/rommelrico/todolistkotlin/FinishToDoActivity.kt
@@ -0,0 +1,12 @@
+package com.rommelrico.todolistkotlin
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class FinishToDoActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_finish_to_do)
+    }
+}
