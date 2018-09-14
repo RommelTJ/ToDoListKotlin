74336eb85ec74e0e52c2910c901eb4ef18af6709
Rommel Rico <me@rommelrico.com>
2018-09-14 08:33:18 -0700

    Getting the selected ToDoItem

--- app/src/main/java/com/rommelrico/todolistkotlin/MainActivity.kt
+++ app/src/main/java/com/rommelrico/todolistkotlin/MainActivity.kt
@@ -34,7 +34,7 @@ class MainActivity : AppCompatActivity() {
         // Creating ListView Adapter and adding it to our toDoItemListView list view.
         val listView = findViewById<ListView>(R.id.toDoItemListView)
         listView.setOnItemClickListener { adapter,view, i, l ->
-            println(i)
+            val selectedToDo = results[i]
         }
         val adapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, results)
         listView.adapter = adapter
