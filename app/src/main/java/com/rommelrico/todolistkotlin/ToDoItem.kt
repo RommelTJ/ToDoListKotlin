0bb82d4dc577a4badd6e78d055a73bc67bb59d45
Rommel Rico <me@rommelrico.com>
2018-09-14 08:38:07 -0700

    Adding a primary key to the ToDoItem

--- app/src/main/java/com/rommelrico/todolistkotlin/ToDoItem.kt
+++ app/src/main/java/com/rommelrico/todolistkotlin/ToDoItem.kt
@@ -1,8 +1,12 @@
 package com.rommelrico.todolistkotlin
 
 import io.realm.RealmObject
+import io.realm.annotations.PrimaryKey
+import java.util.*
 
 open class ToDoItem: RealmObject() {
+    @PrimaryKey
+    var id = UUID.randomUUID().toString()
     var name = ""
     var important = false
 
